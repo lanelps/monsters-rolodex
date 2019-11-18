210e206abd19785109f373dc797f94c3599fed27
Lane le Prevost-Smith <lanelps@gmail.com>
2019-11-18 14:27:56 +1300

    init

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./search-box.styles.css";
+
+export default function SearchBox({ placeholder, handleChange }) {
+  return (
+    <input
+      type="search"
+      placeholder={placeholder}
+      onChange={handleChange}
+      className="search"
+    />
+  );
+}
