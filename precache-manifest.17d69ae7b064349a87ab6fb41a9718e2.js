38b02234d4bb4e0031c65c6a011067615ecd0aac
Lane le Prevost-Smith <lanelps@gmail.com>
2019-11-18 14:28:46 +1300

    Updates

---
+++ precache-manifest.17d69ae7b064349a87ab6fb41a9718e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca20b1c98ee5f9e7e8e758dd468495c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "14d34a2f15475c4f0054",
+    "url": "/monsters-rolodex/static/css/main.49bc1ddd.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "14d34a2f15475c4f0054",
+    "url": "/monsters-rolodex/static/js/main.345b4d86.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
